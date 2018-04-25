8ece0e6403fd93bf0349126b5c5ca94c66f94d4c
Theophilus Omorebee <theo4u@ymail.com>
2018-04-25 01:18:31 +0100

    feat(ui): layouting done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IMessage, MessageType, CloseType, NgAlertService } from '@theo4u/ng-alert';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  message: IMessage;
+  closeTypes = CloseType;
+
+  constructor(private _ngAlert: NgAlertService) {
+
+  }
 }
---
+++ src/app/create-employee/create-employee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styles: []
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
